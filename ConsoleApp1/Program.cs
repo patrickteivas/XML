e766e3df37166021213978d117cdff690f1e75a2
patrickteivas <teivas.2011@mail.ru>
2018-05-03 09:49:20 +0300

    Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
@@ -72,7 +74,7 @@ namespace ConsoleApp1
             //xmlWriter.WriteEndDocument();
             //xmlWriter.Close();
             ////////////////////////////////////////////////////////////////////////////////////////////////////////
-            
+
             //XmlDocument xmlDoc = new XmlDocument();
             //XmlNode rootNode = xmlDoc.CreateElement("users");
             //xmlDoc.AppendChild(rootNode);
@@ -94,15 +96,49 @@ namespace ConsoleApp1
             //xmlDoc.Save("test-doc.xml");
             ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load("test-doc.xml");
-            XmlNodeList userNodes = xmlDoc.SelectNodes("//users/user");
-            foreach (XmlNode userNode in userNodes)
-            {
-                int age = int.Parse(userNode.Attributes["age"].Value);
-                userNode.Attributes["age"].Value = (age + 1).ToString();
-            }
-            xmlDoc.Save("test-doc.xml");
+            //XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.Load("test-doc.xml");
+            //XmlNodeList userNodes = xmlDoc.SelectNodes("//users/user");
+            //foreach (XmlNode userNode in userNodes)
+            //{
+            //    int age = int.Parse(userNode.Attributes["age"].Value);
+            //    userNode.Attributes["age"].Value = (age + 1).ToString();
+            //}
+            //xmlDoc.Save("test-doc.xml");
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //var paljuTooteid = new List<Toode>();
+            //var toode1 = new Toode() { Nimi = "Banaan", Hind = "12" };
+            //var toode2 = new Toode() { Nimi = "Leib", Hind = "4" };
+            //var toode3 = new Toode() { Nimi = "Sai", Hind = "42" };
+            //var toode4 = new Toode() { Nimi = "Fanta", Hind = "52" };
+            //var toode5 = new Toode() { Nimi = "Pastakas", Hind = "222" };
+
+            //paljuTooteid.Add(toode1);
+            //paljuTooteid.Add(toode2);
+            //paljuTooteid.Add(toode3);
+            //paljuTooteid.Add(toode4);
+            //paljuTooteid.Add(toode5);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////KIRJUTAMINE
+
+            //var serializer = new XmlSerializer(paljuTooteid.GetType());
+            //using (var writer = XmlWriter.Create("tooted.xml"))
+            //{
+            //    serializer.Serialize(writer, paljuTooteid);
+            //}
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////LUGEMINE
+
+            //var tooted = new List<Toode>();
+            //var serializer = new XmlSerializer(typeof(List<Toode>));
+            //using (var reader = XmlReader.Create("tooted.xml"))
+            //{
+            //    tooted = (List<Toode>)serializer.Deserialize(reader);
+            //}
+
+            //foreach (var item in tooted)
+            //{
+            //    Console.WriteLine(item.Nimi + " " + item.Hind);
+            //}
         }
     }
 }
---
+++ ConsoleApp1/Toode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Toode
+    {
+        public string Nimi { get; set; }
+        public string Hind { get; set; }
+    }
+}
