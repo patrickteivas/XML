d91dcf79e4b0443f8209a2abfb3f1eaf2031117f
patrickteivas <teivas.2011@mail.ru>
2018-05-03 08:58:58 +0300

    Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
             //XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
             //while (xmlReader.Read())
             //{
@@ -24,6 +25,7 @@ namespace ConsoleApp1
             //        }
             //    }
             //}
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             //XmlDocument xmlDoc = new XmlDocument();
             //xmlDoc.Load("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
@@ -31,26 +33,31 @@ namespace ConsoleApp1
             //{
             //    Console.WriteLine(xmlNode.Attributes["currency"].Value + " " + xmlNode.Attributes["rate"].Value);
             //}
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            XmlDocument xmlDoc = new XmlDocument();
-            //xmlDoc.LoadXml("<user name=\"John Doe\">A user node </user>");
-            //Console.WriteLine(xmlDoc.DocumentElement.Name);
-            //Console.WriteLine(xmlDoc.DocumentElement.InnerText);
+            //XmlDocument xmlDoc = new XmlDocument();
+            ////xmlDoc.LoadXml("<user name=\"John Doe\">A user node </user>");
+            ////Console.WriteLine(xmlDoc.DocumentElement.Name);
+            ////Console.WriteLine(xmlDoc.DocumentElement.InnerText);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //xmlDoc.LoadXml("<users><user>InnderText/InnerXaml</user></users>");
-            //Console.WriteLine(xmlDoc.DocumentElement.InnerXml);
-            //Console.WriteLine(xmlDoc.DocumentElement.InnerText);
-            //Console.WriteLine(xmlDoc.DocumentElement.OuterXml);
+            ////xmlDoc.LoadXml("<users><user>InnderText/InnerXaml</user></users>");
+            ////Console.WriteLine(xmlDoc.DocumentElement.InnerXml);
+            ////Console.WriteLine(xmlDoc.DocumentElement.InnerText);
+            ////Console.WriteLine(xmlDoc.DocumentElement.OuterXml);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //xmlDoc.LoadXml("<user name=\"John Doe\" age=\"42\"/>");
-            //if (xmlDoc.DocumentElement.Attributes["name"] != null)
-            //    Console.WriteLine(xmlDoc.DocumentElement.Attributes["name"].Value);
-            //if(xmlDoc.DocumentElement.Attributes["age"] != null)
-            //    Console.WriteLine(xmlDoc.DocumentElement.Attributes["age"].Value);
+            ////xmlDoc.LoadXml("<user name=\"John Doe\" age=\"42\"/>");
+            ////if (xmlDoc.DocumentElement.Attributes["name"] != null)
+            ////    Console.WriteLine(xmlDoc.DocumentElement.Attributes["name"].Value);
+            ////if(xmlDoc.DocumentElement.Attributes["age"] != null)
+            ////    Console.WriteLine(xmlDoc.DocumentElement.Attributes["age"].Value);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //var path = XDocument.Load("../../example.xml").Element("users").Element("user");
-            //var fromFile = path.Attribute("name").Value + "\n" + path.Attribute("age").Value;
-            //Console.WriteLine(fromFile);
+            ////var path = XDocument.Load("../../example.xml").Element("users").Element("user");
+            ////var fromFile = path.Attribute("name").Value + "\n" + path.Attribute("age").Value;
+            ////Console.WriteLine(fromFile);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             //XmlWriter xmlWriter = XmlWriter.Create("test.xml");
             //xmlWriter.WriteStartDocument();
@@ -64,6 +71,38 @@ namespace ConsoleApp1
             //xmlWriter.WriteString("Jane Doe");
             //xmlWriter.WriteEndDocument();
             //xmlWriter.Close();
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            //XmlDocument xmlDoc = new XmlDocument();
+            //XmlNode rootNode = xmlDoc.CreateElement("users");
+            //xmlDoc.AppendChild(rootNode);
+
+            //XmlNode userNode = xmlDoc.CreateElement("user");
+            //XmlAttribute attribute = xmlDoc.CreateAttribute("age");
+            //attribute.Value = "42";
+            //userNode.Attributes.Append(attribute);
+            //userNode.InnerText = "John Doe";
+            //rootNode.AppendChild(userNode);
+
+            //userNode = xmlDoc.CreateElement("user");
+            //attribute = xmlDoc.CreateAttribute("age");
+            //attribute.Value = "16";
+            //userNode.Attributes.Append(attribute);
+            //userNode.InnerText = "Khenno Pajunurm";
+            //rootNode.AppendChild(userNode);
+
+            //xmlDoc.Save("test-doc.xml");
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("test-doc.xml");
+            XmlNodeList userNodes = xmlDoc.SelectNodes("//users/user");
+            foreach (XmlNode userNode in userNodes)
+            {
+                int age = int.Parse(userNode.Attributes["age"].Value);
+                userNode.Attributes["age"].Value = (age + 1).ToString();
+            }
+            xmlDoc.Save("test-doc.xml");
         }
     }
 }
